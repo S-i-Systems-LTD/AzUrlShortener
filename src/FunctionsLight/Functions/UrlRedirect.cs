1112a4948f7d6a8e3d82c14dec4c18363d3db983
VigneshBarathan <97393074+VigneshBarathan@users.noreply.github.com>
2025-09-23 23:32:06 +0530

    Update UrlRedirect.cs - added logger info

--- src/FunctionsLight/Functions/UrlRedirect.cs
+++ src/FunctionsLight/Functions/UrlRedirect.cs
@@ -24,23 +24,43 @@ namespace Cloud5mins.ShortenerTools.Functions
         }
 
         [Function("UrlRedirect")]
-        public async Task<HttpResponseData> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "shorturl/{shortUrl?}")]
-            HttpRequestData req,
-            string? shortUrl,
-            ExecutionContext context)
-        {
-            _logger.LogDebug("Function reached");
-            UrlServices UrlServices = new UrlServices(_logger, new AzStrorageTablesService(_tblClient));
-            _logger.LogDebug("Services created");
-            _logger.LogDebug($"Redirecting {shortUrl}");
-            string redirectUrl = await UrlServices.Redirect(shortUrl);
-            _logger.LogDebug("got the redirect url");
-            var res = req.CreateResponse(HttpStatusCode.Redirect);
-            res.Headers.Add("Location", redirectUrl);
-            _logger.LogDebug("response created");
-            return res;
+public async Task<HttpResponseData> Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "shorturl/{shortUrl?}")]
+    HttpRequestData req,
+    string? shortUrl,
+    ExecutionContext context)
+{
+    _logger.LogDebug("Function reached");
 
-        }
+    try
+    {
+        _logger.LogDebug($"Received shortUrl: {shortUrl}");
+
+        // Initialize services
+        UrlServices UrlServices = new UrlServices(_logger, new AzStrorageTablesService(_tblClient));
+        _logger.LogDebug("Services created");
+
+        // Attempt to get redirect URL
+        string redirectUrl = await UrlServices.Redirect(shortUrl);
+        _logger.LogDebug($"Redirect URL obtained: {redirectUrl}");
+
+        // Create redirect response
+        var res = req.CreateResponse(HttpStatusCode.Redirect);
+        res.Headers.Add("Location", redirectUrl);
+        _logger.LogDebug("Redirect response created");
+
+        return res;
+    }
+    catch (Exception ex)
+    {
+        _logger.LogError($"UrlRedirect failed: {ex}");
+
+        // Return 500 with exception message for debugging
+        var errorResponse = req.CreateResponse(HttpStatusCode.InternalServerError);
+        await errorResponse.WriteStringAsync($"Error in UrlRedirect: {ex.Message}");
+        return errorResponse;
+    }
+}
+
     }
 }
